5a54aada5d493c29a39e095d2a8864667cd73c48
zhenglijun <392090057@qq.com>
2025-12-04 17:17:39 +0800

    更新Blog站点配置

--- 6_WebSite/Blogs.WebSite/vite.config.js
+++ 6_WebSite/Blogs.WebSite/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/website/' //部署配置，当前部署在容器中并且通过域名zhenglijun.com/website来访问，如果使用子域名则不需要配置
+  base: '/' //部署配置，当前部署在容器中并且通过域名zhenglijun.com/website来访问，如果使用子域名则不需要配置
 })
